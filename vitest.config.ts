1ba13271831be4e71050407f796fab276a30d763
Alberto De Agostini <alberto.deagostini@infinitaslearning.com>
2024-01-19 17:32:27 +0100

    chore: use different action

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      reporter: ['html', 'json-summary', 'json'],
       provider: 'v8',
     },
   },
